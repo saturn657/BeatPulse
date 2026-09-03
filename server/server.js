b8692d70b4aaafb3d45153280af77b439c37ad4a
Mohit  Suri <saturn657@proton.me>
2026-09-03 11:21:43 +0530

    initialize Express server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express=require("express")
+
+const app=express()
+const PORT=5000
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.json({message:"BeatPulse server is running"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`BeatPulse server running on port ${PORT}`)
+})
