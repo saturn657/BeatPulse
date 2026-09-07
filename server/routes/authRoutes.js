f7c5b4a4bacf429cdb3f5ee493d96150b277338f
Mohit  Suri <saturn657@proton.me>
2026-09-07 21:42:29 +0530

    create authentication middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express=require("express")
 const {registerUser,loginUser}=require("../controllers/authController")
+const protect=require("../middleware/authMiddleware")
 
 const router=express.Router()
 
 router.post("/register",registerUser)
 router.post("/login",loginUser)
+
+router.get("/me",protect,(req,res)=>{
+    res.json(req.user)
+})
 
 module.exports=router
