4766e9d0264198314bafb43c1fa91b3932cd7b51
Mohit  Suri <saturn657@proton.me>
2026-09-12 23:51:40 +0530

    add music search API

--- server/controllers/musicController.js
+++ server/controllers/musicController.js
@@ -29,7 +29,22 @@ const addMusic=async(req,res)=>{
 
 const getMusic=async(req,res)=>{
     try{
-        const music=await Music.find().sort({createdAt:-1})
+        const search=req.query.search
+
+let music
+
+if(search){
+    music=await Music.find({
+        $or:[
+            {title:{$regex:search,$options:"i"}},
+            {artist:{$regex:search,$options:"i"}},
+            {album:{$regex:search,$options:"i"}},
+            {genre:{$regex:search,$options:"i"}}
+        ]
+    }).sort({createdAt:-1})
+}else{
+    music=await Music.find().sort({createdAt:-1})
+}
         res.json(music)
     }catch(error){
         res.status(500).json({message:"Failed to fetch music"})
