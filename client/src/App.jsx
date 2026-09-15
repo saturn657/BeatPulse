799c39f71644c387f176ea87fb1ba6bbf6af7834
Mohit  Suri <saturn657@proton.me>
2026-09-16 05:13:01 +0530

    create BeatPulse main layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 function App(){
   return(
-    <div className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold">BeatPulse</h1>
-        <p className="mt-3 text-zinc-400">Your music, your pulse.</p>
-      </div>
+    <div className="min-h-screen bg-zinc-950 text-white flex">
+      <aside className="w-64 bg-zinc-900 border-r border-zinc-800 p-6">
+        <h1 className="text-2xl font-bold">BeatPulse</h1>
+
+        <nav className="mt-10 space-y-4">
+          <p className="text-zinc-300">Home</p>
+          <p className="text-zinc-400">Search</p>
+          <p className="text-zinc-400">Your Library</p>
+          <p className="text-zinc-400">Favorites</p>
+        </nav>
+      </aside>
+
+      <main className="flex-1 p-8">
+        <h2 className="text-3xl font-bold">Welcome to BeatPulse</h2>
+        <p className="mt-2 text-zinc-400">
+          Your music, your pulse.
+        </p>
+      </main>
     </div>
   )
 }
